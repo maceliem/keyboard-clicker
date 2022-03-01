ecac6e51526f440c611ffe4042862ba27e66855b
maceliem <mattias@kirkaune.com>
2022-03-01 10:47:50 +0100

    Added æøå and animation

--- Scripts/keyboard.js
+++ Scripts/keyboard.js
@@ -1,13 +1,14 @@
 let clicks = 0
 
-document.addEventListener("keypress", function (event) {
-    let key = event.key.toUpperCase()
-    document.getElementById(key).classList.add("pressed")
-    clicks++
-    this.getElementById("clicks").innerHTML = `Clicks: ${clicks}`
-});
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener("keypress", function (event) {
+    let clicks = 0    
     let key = event.key.toUpperCase()
-    document.getElementById(key).classList.remove("pressed")
+    if (!key.match(/[A-Z]|[ÆØÅ]/)) return
+    let element = document.getElementById(key)
+    element.classList.add("pressed")
+    element.style.display = "block"
+    clicks++
+    document.getElementById("clicks").innerHTML = `Clicks: ${clicks}`
+    setTimeout(() => { document.getElementById(key).classList.remove("pressed") }, 500)
 });
